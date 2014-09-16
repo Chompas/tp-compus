4844af62cccf971a27e7c263524e5bd3204e2519
Emiliano Viscarra <chompas@gmail.com>
2014-09-16 12:01:38 -0300

    Indentando

--- tp0.c
+++ tp0.c
@@ -1,3 +1,4 @@
+// TP 0 - Mandelbrot
 #include <getopt.h>
 #include <stdio.h>
 #include <string.h>
@@ -6,9 +7,6 @@
 #define TP0_VERSION "Version 1.0 Tp 0 - 6620 Organizacion de Computadoras"
 #define FALSE 0
 #define TRUE  1
-
-// TP 0 - Mandelbrot
-
 
 // Global constants
 static const int iterations = 50;
@@ -42,7 +40,7 @@ int mandelbrot(double cRe, double cIm) {
 
 void initPGM(FILE *pgmFile, int width, int height) {
 
-  fprintf(pgmFile, "P5");
+  fprintf(pgmFile, "P2");
   fprintf(pgmFile,"\n");
   fprintf(pgmFile, "%d %d", width, height);
   fprintf(pgmFile,"\n");
@@ -50,24 +48,22 @@ void initPGM(FILE *pgmFile, int width, int height) {
   fprintf(pgmFile,"\n");
 }
 
-void usage ()
-{
-  	fprintf( stderr," Usage:\n");
-   fprintf( stderr,"\t tp0 -h\n");
-   fprintf( stderr,"\t tp0 -v\n");
-   fprintf( stderr,"\t tp0 -c 0+0i -r 1x1 -o -\n");
-   fprintf( stderr," Options:\n");
-   fprintf( stderr,"\t -v, --version \t Show version string.\n");
-   fprintf( stderr,"\t -h, --help \t Print this message and quit.\n");
-   fprintf( stderr,"\t -r, --resolution \t Set image resolution to WxH pixels.\n");
-   fprintf( stderr,"\t -c, --center \t Set center of the image, expressed in binomial form a+bi.\n");
-   fprintf( stderr,"\t -w, --width \t Set width of the rectangle of the complex plane.\n");
-   fprintf( stderr,"\t -H, --height \t Set height of the rectangle of the complex plane.\n");
-   fprintf( stderr,"\t -o, --output \t Path to output file.\n");
+void usage () {
+  fprintf( stderr," Usage:\n");
+  fprintf( stderr,"\t tp0 -h\n");
+  fprintf( stderr,"\t tp0 -v\n");
+  fprintf( stderr,"\t tp0 -c 0+0i -r 1x1 -o -\n");
+  fprintf( stderr," Options:\n");
+  fprintf( stderr,"\t -v, --version \t Show version string.\n");
+  fprintf( stderr,"\t -h, --help \t Print this message and quit.\n");
+  fprintf( stderr,"\t -r, --resolution \t Set image resolution to WxH pixels.\n");
+  fprintf( stderr,"\t -c, --center \t Set center of the image, expressed in binomial form a+bi.\n");
+  fprintf( stderr,"\t -w, --width \t Set width of the rectangle of the complex plane.\n");
+  fprintf( stderr,"\t -H, --height \t Set height of the rectangle of the complex plane.\n");
+  fprintf( stderr,"\t -o, --output \t Path to output file.\n");
 }
 
-static struct option const longopts[] =
-{
+static struct option const longopts[] = {
   {"resolution", required_argument, 0, 'r'},
   {"center", required_argument, 0, 'c'},
   {"width", required_argument, 0, 'w'},
@@ -108,122 +104,130 @@ int main(int argc, char **argv) {
   short int o_value = FALSE;
   FILE *fout;
 
-  while ((optc = getopt_long (argc, argv, "hvr:c:w:H:o:", longopts, (int *) 0)) != EOF)
-  {
-    switch (optc)
-    {
+  while ((optc = getopt_long (argc, argv, "hvr:c:w:H:o:", longopts, (int *) 0)) != EOF) {
+    switch (optc) {
       case 'h':
-      usage();
-      return 0;
-		break;
+        usage();
+        return 0;
+        break;
 
-		case 'v':
-      fprintf (stderr, "\n" );
-      fprintf (stderr, TP0_VERSION );
-      fprintf (stderr, "\n\n" );
-      return 0;
-		break;
+      case 'v':
+        fprintf (stderr, "\n" );
+        fprintf (stderr, TP0_VERSION );
+        fprintf (stderr, "\n\n" );
+        return 0;
+        break;
 
       case 'r':
-      res = optarg;
-      res_value = TRUE;
-		break;
+        res = optarg;
+        res_value = TRUE;
+        break;
 
       case 'c':
-		center = optarg;
-		center_value = TRUE;
-		break;
+        center = optarg;
+        center_value = TRUE;
+        break;
 
       case 'w':
-		w = atoi(optarg);
-		break;
+        w = atoi(optarg);
+        break;
 
-		case 'H':
-		h = atoi(optarg);
-		break;
+      case 'H':
+        h = atoi(optarg);
+        break;
 
-		case 'o':
-		output = optarg;
-		o_value = TRUE;
-		break;
+      case 'o':
+        output = optarg;
+        o_value = TRUE;
+        break;
 
-		case '?':
-		fprintf(stderr, "Opcion invalida-%c\n", optc);
-      return 1;
+      case '?':
+        fprintf(stderr, "Opcion invalida-%c\n", optc);
+        return 1;
 
       default:
-		usage ();
-		return 0;
-     }
+        usage ();
+        return 0;
+    }
   }
 
   /*Resolucion*/
-  if(res_value == TRUE){
+  if(res_value == TRUE) {
     resx = strstr( res, "x" );
+
     if ( resx == NULL ||  /* Si nos tiene x */
-         resx == res || /* Si la x esta al principio */
-         resx == &res[strlen(res)-1] ) /* Si la x esta al final */
-    {
-        fprintf(stderr, "Error al parsear resolucion -%s\n", res);
-        return 0;
+        resx == res || /* Si la x esta al principio */
+        resx == &res[strlen(res)-1] ) { /* Si la x esta al final */
+
+          fprintf(stderr, "Error al parsear resolucion -%s\n", res);
+          return 0;
     }
-    /* width */
+
+    // Width
     resx = strstr( res, "x" );
     char *p = res;
     int i = 0;
     char num[32];
+
     while( p!=NULL && p != resx ) {
       num[i] = *p; i++; p++;
     }
+
     num[i] = 0;
     width = atoi( num );
 
-    /* height */
+    // Height
     i = 0;
     resx = &res[strlen(res)-1];
+
     while ( p!=NULL && p != resx ) {
       p++; num[i] = *p; i++;
     }
+
     num[i] = 0;
     height = atoi( num );
     p++;
 
   	}
 
-  	//Center
+  	// Center
   	if(center_value == TRUE) {
   		cen = strstr(center,"+");
   		if(cen == NULL || /*Si no tiene el + */
   			cen == center || /*Si el + esta al principio */
   			cen == &center[strlen(center)-1]){ /*Si el + esta al final*/
-  			fprintf(stderr, "Error al parsear centro -%s\n", center);
-         return 0;
-  			}
+          fprintf(stderr, "Error al parsear centro -%s\n", center);
+          return 0;
+  		}
 
   		cen = strstr(center,"i");
   		if(cen == NULL || /*Si no tiene el i */
   			cen != &center[strlen(center)-1]){ /*Si el i no esta al final*/
-  			fprintf(stderr, "Error al parsear centro -%s\n", center);
-         return 0;
+          fprintf(stderr, "Error al parsear centro -%s\n", center);
+          return 0;
   			}
 
-  		/*Parte real*/
+  		// Parte real
   		cen = strstr( center, "+" );
       char *p = center;
       int i = 0;
       char num[32];
+
       while( p!=NULL && p != cen ) {
         num[i] = *p; i++; p++;
       }
+
       num[i] = 0;
       centerRe = atoi( num );
 
-      /*Parte imaginaria*/
+      // Parte imaginaria
     	i = 0;
     	cen = &center[strlen(center)-2];
+
     	while ( p!=NULL && p != cen ) {
       	p++; num[i] = *p; i++;
     	}
+
     	num[i] = 0;
     	centerIm = atoi( num );
     	p++;
@@ -235,19 +239,18 @@ int main(int argc, char **argv) {
   	minIm = centerIm-(h/2);
 
   // File
-  if(o_value == TRUE){
-  	if (strlen( output )==1 && *output=='-' ){
-  	   fout = stdout;
-  	}
-   else{
-   	fout = fopen( output, "wb");
-  	}
-   if ( fout == NULL ){
+  if(o_value == TRUE) {
+  	if (strlen( output )==1 && *output=='-' ) {
+      fout = stdout;
+    } else {
+      fout = fopen( output, "wb");
+    }
+
+    if ( fout == NULL ) {
       fprintf(stderr, "Error al abrir archivo output -%s\n", output);
       return 0;
-	}
-  }
-  else{
+    }
+  } else {
   	fout = stdout;
   }
 
